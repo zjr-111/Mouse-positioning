5966aa89c5e7db12cf657790a07cf40a4a05f17c
zjr-111 <127306838+zjr-111@users.noreply.github.com>
2023-09-19 20:49:18 +0800

    Add files via upload

---
+++ positioning.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 10 16:39:23 2021
+
+@author: Lenovo
+"""
+#安卓手机拍的照片，下载下来放到桌面就行
+
+ 
+import requests
+import exifread
+ 
+ 
+class GetPhotoInfo:
+    def __init__(self, photo):
+        self.photo = photo
+        # 百度地图ak
+        self.ak = '3iVKI77ap5Tb2M2FIGS1gvEtcSzD3yWZ'
+        self.location = self.get_photo_info()
+ 
+    def get_photo_info(self, ):
+        with open(self.photo, 'rb') as f:
+            tags = exifread.process_file(f)
+        try:
+            # 打印照片其中一些信息
+            print('拍摄时间：', tags['EXIF DateTimeOriginal'])
+            print('照相机制造商：', tags['Image Make'])
+            print('照相机型号：', tags['Image Model'])
+            print('照片尺寸：', tags['EXIF ExifImageWidth'], tags['EXIF ExifImageLength'])
+            # 拿到的是度分秒的经纬度，需要转化为十进制的经纬度
+            # 转换公式为    度 + 分 / 60 + 秒 / 3600
+            # 纬度
+            lat_ref = tags["GPS GPSLatitudeRef"].printable
+            lat = tags["GPS GPSLatitude"].printable[1:-1].replace(" ", "").replace("/", ",").split(",")
+            lat = float(lat[0]) + float(lat[1]) / 60 + float(lat[2]) / float(lat[3]) / 3600
+            if lat_ref != "N":
+                lat = lat * (-1)
+            # 经度
+            lon_ref = tags["GPS GPSLongitudeRef"].printable
+            lon = tags["GPS GPSLongitude"].printable[1:-1].replace(" ", "").replace("/", ",").split(",")
+            lon = float(lon[0]) + float(lon[1]) / 60 + float(lon[2]) / float(lon[3]) / 3600
+            if lon_ref != "E":
+                lon = lon * (-1)
+        except KeyError:
+            return "ERROR:请确保照片包含经纬度等EXIF信息。"
+        else:
+            print("经纬度：", lat, lon)
+            return lat, lon
+ 
+    def get_location(self):
+        url = 'http://api.map.baidu.com/reverse_geocoding/v3/?ak={}&output=json' \
+              '&coordtype=wgs84ll&location={},{}'.format(self.ak, *self.location)
+        response = requests.get(url).json()
+        status = response['status']
+        if status == 0:
+            address = response['result']['formatted_address']
+            print('详细地址：', address)
+        else:
+            print('baidu_map error')
+ 
+ 
+if __name__ == '__main__':
+    Main = GetPhotoInfo('111.jpg')
+    Main.get_location()
